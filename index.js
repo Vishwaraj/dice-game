1bc4d7ac67c19b4219d0207b495f43a105bdfa1d
Vishwaraj Kamble <vishwarajk96@gmail.com>
2022-01-05 22:11:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1 );
+
+var randomdiceimage = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomdiceimage);
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1 );
+
+var randomdiceimage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomdiceimage2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerText = "Player 1 Wins!";
+}
+
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerText = "Player 2 Wins!";
+}
+
+else {
+  document.querySelector("h1").innerText = "Draw!";
+}
